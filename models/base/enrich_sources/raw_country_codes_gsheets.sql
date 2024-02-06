dc5a8c449808b446e84af4c323c4495891ed1c82
chulupidata@gmail.com <chulupidata@gmail.com>
2024-02-06 01:29:02 +0000

    saving

--- models/base/enrich_sources/raw_country_codes_gsheets.sql
+++ models/base/enrich_sources/raw_country_codes_gsheets.sql
@@ -1,6 +1,11 @@
-with raw
-from
-    (
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+with
+    raw as (
         select
             country_simplified as country,
             name as country_full_name,
@@ -16,8 +21,8 @@ from
             currency_code,
             currency_symbol,
             gdp_2021_worldbank,
-            ranking_gdp_2021_worldbank,
+            ranking_gdp_2021_worldbank
         from {{ source("imports", "country_codes") }}
-    ),
+    )
 select *
 from raw
